7e56b7a0c3ce1b2220052343c28889400bc63168
Sajeekanth05 <139636074+Sajeekanth@users.noreply.github.com>
2024-12-25 16:38:28 +0000

    login create

---
+++ php/login.php
@@ -0,0 +1,35 @@
+<?php
+require_once 'dbconf.php';
+
+if (isset($_POST['login'])) {
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+    
+    // Prepare the SQL query to fetch the username and hashed password
+    $sql = "SELECT username, password  FROM user WHERE username = '$username'";
+    $result = $connect->query($sql);
+
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        $hashedPassword = $row["password"]; // Fetch the hashed password
+
+        // Verify the password
+        if (password_verify($password, $hashedPassword)) {
+            //session_start(); // Start the session
+            
+           //$_SESSION["user_name"] = $row["username"];
+            
+            header("Location: ../index.html");
+            exit; // Ensure no further code runs after redirection
+        } else {
+            $error = "Invalid username or password.";
+        }
+    } else {
+        $error = "Invalid username or password.";
+    }
+
+    $connect->close(); // Close the database connection
+    //header("Location: ../login.html?error=" . urlencode($error));
+   // exit;
+}
+?>
